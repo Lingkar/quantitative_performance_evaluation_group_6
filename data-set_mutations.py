f06ecff148fb4d4fc384b81535f460b17ce6fbf4
Jan Buijnsters <buijnstersjan@gmail.com>
2020-10-02 20:34:32 +0200

    Fixed random_duplicate function

--- data-set_mutations.py
+++ data-set_mutations.py
@@ -3,26 +3,22 @@ import idx2numpy
 import math
 import random
 
-from numpy.core._multiarray_umath import ndarray
-
 training_image_file = "./data/train-images-idx3-ubyte"
 training_label_file = "./data/train-labels-idx1-ubyte"
 
 images = idx2numpy.convert_from_file(training_image_file)
-
-print(images.shape)
+dataset_array = images.tolist()
 
 
 def get_random_duplicates(dataset, percentage):
     duplicate_amount = math.floor((len(dataset) * percentage / 100))
     random_indexes = random.sample(range(1, len(dataset)), duplicate_amount)
-    duplicated_images = []
+    result = dataset
     for i in random_indexes:
-        duplicated_images.append(dataset[i])
+        result.append(dataset[i])
 
-    duplicated_images_np = np.asarray(duplicated_images)
-    new_dataset: ndarray = np.append(dataset, duplicated_images_np)
-    return new_dataset
+    return result
 
 
-print(get_random_duplicates(images, 10).shape)
+print(len(get_random_duplicates(dataset_array, 10)))
+
