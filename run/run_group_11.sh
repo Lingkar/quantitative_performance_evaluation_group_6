b5153b5783f9c09ffd8dca10018a3c69037d8ede
bartziengs <a.h.ziengs@student.tudelft.nl>
2020-10-23 19:10:25 +0200

    Update run_group_11.sh

--- run/run_group_11.sh
+++ run/run_group_11.sh
@@ -14,7 +14,6 @@ do
 if ((count>index));then
 ./spark-submit --master spark://10.128.0.3:7077 --driver-cores 2  --driver-memory 1G --total-executor-cores 4 --executor-cores 2 --executor-memory 1G --py-files /home/am72ghiassi/bd/spark/lib/bigdl-0.11.0-python-api.zip,/home/am72ghiassi/bd/quantitative_performance_evaluation_group_6/lenet5.py --properties-file /home/am72ghiassi/bd/spark/conf/spark-bigdl.conf --jars /home/am72ghiassi/bd/spark/lib/bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar --conf spark.driver.extraClassPath=/home/am72ghiassi/bd/spark/lib/bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar --conf spark.executer.extraClassPath=bigdl-SPARK_2.3-0.11.0-jar-with-dependencies.jar  /home/am72ghiassi/bd/quantitative_performance_evaluation_group_6/lenet5.py  --action train --dataPath /home/am72ghiassi/bd/quantitative_performance_evaluation_group_6/data/experiments/repetition_${rep}/le_${le}_in_${in} --kernelSize1 ${ks1} --kernelSize2 ${ks2} --endTriggerNum 20 > /home/am72ghiassi/bd/quantitative_performance_evaluation_group_6/outputs/repetition_${rep}/le_${le}_in_${in}_ks1_${ks1}_ks2_${ks2}.txt
 fi
-echo $count
 ((count++))
 done
 done
