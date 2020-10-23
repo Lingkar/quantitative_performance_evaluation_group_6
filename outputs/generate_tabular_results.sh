a057b5b0363340eca723d40ec7f8c4d1cfe7ddc6
Bart Ziengs <bzi@netcompany.com>
2020-10-23 17:52:17 +0200

    updated analysis

--- outputs/generate_tabular_results.sh
+++ outputs/generate_tabular_results.sh
@@ -1,6 +1,6 @@
 #/!/bin/bash
-echo -e "rep\tle\tin\tks1\tks2\tepochs\tacc\t" 
-for rep in 4
+echo -e "rep\tle\tnoise\tks1\tks2\tepochs\tacc" 
+for rep in 4 5
 do
 for le in 0 20 40
 do
@@ -10,7 +10,7 @@ for ks1 in 3 8
 do 
 for ks2 in 3 8 
 do
-cat ./repetition_${rep}/le_${le}_in_${in}_ks1_${ks1}_ks2_${ks2}.txt | grep Top1 | grep -e 'Epoch 5\|Epoch 10\|Epoch 20' | sed -r "s/.*Epoch\s([[:digit:]]*).*accuracy: ([[:digit:]]\.[[:digit:]]*)\)/ $rep\t$le\t$in\t$ks1\t$ks2\t\1\t\2 /"
+cat ./repetition_${rep}/le_${le}_in_${in}_ks1_${ks1}_ks2_${ks2}.txt | grep Top1 | grep -e 'Epoch 5\|Epoch 10\|Epoch 20' | sed -r "s/.*Epoch\s([[:digit:]]*).*accuracy: ([[:digit:]]\.[[:digit:]]*)\)/ $rep\t$le\t$in\t$ks1\t$ks2\t\1\t\2/"
 done
 done
 done
