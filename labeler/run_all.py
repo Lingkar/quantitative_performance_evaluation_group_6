a2b6b71e1033e0b495dfe4422b30d5b037fac5df
Jan Buijnsters <buijnstersjan@gmail.com>
2020-10-25 12:09:01 +0100

    And added replication_set to output

--- labeler/run_all.py
+++ labeler/run_all.py
@@ -2,11 +2,11 @@ import os
 import gc
 
 
-def saveToFile(labeler, ks, label_error, image_noise, epochs, count, accuracy):
+def saveToFile(labeler, ks, label_error, image_noise, epochs, count, accuracy, replication_set):
     with open("intermediate_results.txt", "a") as result_file:
         result_file.write("\n")
         result_file.write(
-            "Index: " + str(count) + ", labeler: " + str(labeler) + ", ks: " + str(ks) + ", label_error: " + str(
+            "Replication_set: " + str(replication_set) + "Index: " + str(count) + ", labeler: " + str(labeler) + ", ks: " + str(ks) + ", label_error: " + str(
                 label_error) + ", image_noise: " + str(image_noise) + ", epochs: " + str(epochs) + "\n")
         result_file.write(str(accuracy))
 
@@ -30,7 +30,7 @@ if __name__ == "__main__":
                     epochs = e[2]
                     command = 'python3 main.py -s mnist -a ' + str(alpha) + ' -b ' + str(beta) + ' -t ' + str(
                         thr) + ' -ks1 ' + str(j) + ' -ks2 ' + str(j) + ' -e ' + str(epochs) + ' -er ' + str(
-                        label_error) + ' -in ' + str(image_noise) + ' -la ' + str(i)
+                        label_error) + ' -in ' + str(image_noise) + ' -la ' + str(i) + ' -re ' + str(replication_set)
                     print("INDEX: " + str(count))
                     print(command)
                     stream = os.popen(command)
@@ -39,6 +39,6 @@ if __name__ == "__main__":
                     # result_repetitions.append(accuracy)
                     # print(result_repetitions)
                     # if r == repetition - 1:
-                    saveToFile(i, j, label_error, image_noise, epochs, count, accuracy)
+                    saveToFile(i, j, label_error, image_noise, epochs, count, accuracy, replication_set)
                 count = count + 1
     print("Done!")
